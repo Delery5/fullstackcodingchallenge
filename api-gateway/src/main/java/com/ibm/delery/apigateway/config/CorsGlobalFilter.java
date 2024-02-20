f8377dd2bd5ac3350379ae4a11b7b649f91b066a
Delery5 <harrisondelery.com>
2024-02-20 14:49:35 -0600

    Made changes to K8's

--- api-gateway/src/main/java/com/ibm/delery/apigateway/config/CorsGlobalFilter.java
+++ api-gateway/src/main/java/com/ibm/delery/apigateway/config/CorsGlobalFilter.java
@@ -15,30 +15,34 @@ import java.util.List;
 
 @Component
 public class CorsGlobalFilter implements WebFilter {
+        @Override
+        public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
 
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-        ServerHttpRequest request = exchange.getRequest();
-        ServerHttpResponse response = exchange.getResponse();
+            HttpHeaders headers = response.getHeaders();
+            if (!headers.containsKey(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)) {
+                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:4200");
+            } else {
+                List<String> originValues = headers.get(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN);
+                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, originValues.get(0));
+            }
 
-        HttpHeaders headers = response.getHeaders();
-        if (!headers.containsKey(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)) {
-            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:4200");
-        } else {
-            List<String> originValues = headers.get(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN);
-            headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, originValues.get(0));
+            // Add your load balancer's URL to the Access-Control-Allow-Origin header
+            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://34.27.82.55:80");
+
+            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE");
+            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
+            headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization, Cache-Control, Content-Type");
+            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+            headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
+
+            if (request.getMethod() == HttpMethod.OPTIONS) {
+                response.setStatusCode(HttpStatus.OK);
+                return Mono.empty();
+            }
+
+            return chain.filter(exchange);
         }
-        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE");
-        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
-        headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization, Cache-Control, Content-Type");
-        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-        headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
-
-        if (request.getMethod() == HttpMethod.OPTIONS) {
-            response.setStatusCode(HttpStatus.OK);
-            return Mono.empty();
-        }
-
-        return chain.filter(exchange);
     }
 }
