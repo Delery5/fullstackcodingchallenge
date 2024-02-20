e1b176114361a1ee435e4b1f634f35e8ad9b2613
Delery5 <harrisondelery.com>
2024-02-20 12:36:31 -0600

    Made changes to K8's

---
+++ api-gateway/src/main/java/com/ibm/delery/apigateway/config/CorsGlobalFilter.java
@@ -0,0 +1,44 @@
+package com.ibm.delery.apigateway.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+public class CorsGlobalFilter implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+
+        HttpHeaders headers = response.getHeaders();
+        if (!headers.containsKey(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN)) {
+            headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:4200");
+        } else {
+            List<String> originValues = headers.get(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN);
+            headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, originValues.get(0));
+        }
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE");
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
+        headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization, Cache-Control, Content-Type");
+        headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "3600");
+
+        if (request.getMethod() == HttpMethod.OPTIONS) {
+            response.setStatusCode(HttpStatus.OK);
+            return Mono.empty();
+        }
+
+        return chain.filter(exchange);
+    }
+}
