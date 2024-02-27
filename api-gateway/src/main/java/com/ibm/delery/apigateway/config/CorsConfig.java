c36731c5e877fb8a44c21c3ce79cfd17e35866ae
Delery5 <harrisondelery.com>
2024-02-27 10:04:41 -0600

    Made changes to K8's

--- api-gateway/src/main/java/com/ibm/delery/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/ibm/delery/apigateway/config/CorsConfig.java
@@ -1,20 +1,42 @@
 package com.ibm.delery.apigateway.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200","http://35.232.9.232") // Add your allowed origins here
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Add your allowed methods here
-                .allowedHeaders("Authorization", "Content-Type") // Add your allowed headers here
-                .allowCredentials(true); // Allow credentials if needed
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList(
+                "http://35.232.9.232",
+                "*" // Allow requests from any other origin
+        ));
+        corsConfiguration.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name(),
+                HttpMethod.OPTIONS.name()
+        ));
+        corsConfiguration.setAllowedHeaders(Arrays.asList(
+                "Authorization",
+                "Cache-Control",
+                "Content-Type"
+        ));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsFilter(source);
     }
 }
-
-
