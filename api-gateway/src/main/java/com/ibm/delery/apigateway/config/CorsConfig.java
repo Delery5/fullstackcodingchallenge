ef3c3734c4bc35bbc151c44be84db36a221ce30a
Delery5 <harrisondelery.com>
2024-02-27 08:50:26 -0600

    Made changes to K8's

--- api-gateway/src/main/java/com/ibm/delery/apigateway/config/CorsConfig.java
+++ api-gateway/src/main/java/com/ibm/delery/apigateway/config/CorsConfig.java
@@ -10,10 +10,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://35.232.9.232")
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedOrigins("http://localhost:4200","http://35.232.9.232") // Add your allowed origins here
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Add your allowed methods here
+                .allowedHeaders("Authorization", "Content-Type") // Add your allowed headers here
+                .allowCredentials(true); // Allow credentials if needed
     }
 }
 
